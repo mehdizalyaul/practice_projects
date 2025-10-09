7ac129100ae20d8b9a82efd79bcb50fa80764779
Mehdi ZALYAUL <mehdi.zalyaul@gmail.com>
2025-10-09 23:59:56 +0100

    feat: add dark and light mode toggle in the Navbar

--- src/PersonalDashboard/components/Navbar.jsx
+++ src/PersonalDashboard/components/Navbar.jsx
@@ -1,15 +1,34 @@
 import { NavLink } from "react-router-dom";
 import "../styles/Navbar.css";
+import { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext";
 
 export default function Navbar() {
+  const { theme, toggleTheme } = useContext(ThemeContext);
   return (
     <nav>
-      <NavLink to="/" end>
-        Dashboard
-      </NavLink>
-      <NavLink to="/tasks">Tasks</NavLink>
-      <NavLink to="/profiles">Profiles</NavLink>
-      <NavLink to="/settings">Settings</NavLink>
+      <div>
+        <NavLink to="/" end>
+          Dashboard
+        </NavLink>
+        <NavLink to="/tasks">Tasks</NavLink>
+        <NavLink to="/profiles">Profiles</NavLink>
+        <NavLink to="/settings">Settings</NavLink>
+      </div>
+
+      <div>
+        <input
+          type="checkbox"
+          id="checkbox"
+          className="checkbox"
+          onChange={toggleTheme}
+        />
+        <label htmlFor="checkbox" className="checkbox-label">
+          <i className="fas fa-moon"></i>
+          <i className="fas fa-sun"></i>
+          <span className="ball"></span>
+        </label>
+      </div>
     </nav>
   );
 }
